44ecb30eb29f31399c253d6f78358286d9c36bdb
cazdemun <cplu1509@gmail.com>
2020-05-05 17:18:45 -0500

    python server v1

---
+++ ci_pipeline.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Transpiling
+
+# npm run tsc
+
+# Updating
+
+# https://help.github.com/en/github/using-git/caching-your-github-password-in-git
+git config --global credential.helper 'cache --timeout=86400'
+
+echo $1
+git add .
+git commit -m "$1"
+
+git push
+
+# Deploying
+
+ssh -tt -i /home/charles/.ssh/amazon-tesis.pem ubuntu@18.219.150.69 << EOF
+cd apps/bci-backend
+git config --global credential.helper 'cache --timeout=86400'
+git pull
+pm2 restart server.py
+exit
+EOF
